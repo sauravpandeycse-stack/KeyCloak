97080cec979c213d8ca1ac3e6ca6878e85786296
sauravpandey <sauravpandeycs@gmail.com>
2023-10-11 10:04:59 +0530

    KeyCloakDemo

---
+++ src/main/java/com/sauravNG/demo/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.sauravNG.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/user1")
+    public String testing1(){
+        return "success for user 1";
+    }
+
+    @GetMapping("/user2")
+    public String testing2(){
+        return "success for user 2";
+    }
+}
